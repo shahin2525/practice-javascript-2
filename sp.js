339dafab1935d83a395f9f13ec9f16668c86a4e5
shahin2525 <shahin451999@gmail.com>
2025-08-09 11:13:12 +0600

    practice pb-2

--- sp.js
+++ sp.js
@@ -6,4 +6,11 @@ const Bangle = 99.5;
 
 const TotalMarks = math + Biology + Chemistry + Physics + Bangle;
 const averageMarks = parseFloat((TotalMarks / 5).toFixed(2));
-console.log(averageMarks);
+//
+
+const input = 119;
+const givenNumber = 5;
+const reminder = input % givenNumber;
+const a = isNaN("11");
+const b = isNaN("rakib");
+console.log(b);
