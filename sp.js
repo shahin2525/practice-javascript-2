8b1123e19c8b1b1b8e4394d53934c19728488e9e
shahin2525 <shahin451999@gmail.com>
2025-08-09 10:02:28 +0600

    practice proble 1

---
+++ sp.js
@@ -0,0 +1,9 @@
+const math = 75.25;
+const Biology = 65;
+const Chemistry = 80;
+const Physics = 35.45;
+const Bangle = 99.5;
+
+const TotalMarks = math + Biology + Chemistry + Physics + Bangle;
+const averageMarks = parseFloat((TotalMarks / 5).toFixed(2));
+console.log(averageMarks);
