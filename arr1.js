4eb9db9738037210f3dd4a87940301f7c1fe7256
shahin2525 <shahin451999@gmail.com>
2025-08-17 20:21:36 +0600

    practice array

---
+++ arr1.js
@@ -0,0 +1,10 @@
+const array = ["rakib", "sakib", 20, "20"];
+// console.log(array[0]);
+// array[0] = "makib";
+// console.log(array[0]);
+
+if (array.includes("ssakib")) {
+  console.log("you are cricketer");
+} else {
+  console.log("yor are normal");
+}
