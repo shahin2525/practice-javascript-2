d2667fa7a43f48b0aafde7a8b7f361669e99edb5
shahin2525 <shahin451999@gmail.com>
2025-08-21 13:32:21 +0600

    practice incremental loop

--- pr2/loop.js
+++ pr2/loop.js
@@ -4,6 +4,27 @@
 //   i = i + 2;
 // }
 
-for (let i = 1; i <= 10; i = i + 2) {
-  console.log(i);
+// for (let i = 1; i <= 10; i = i + 2) {
+//   console.log(i);
+// }
+
+// decremental loop
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+let i = 1;
+while (i <= 30) {
+  if (i % 5 === 0) {
+    console.log(i);
+  }
+
+  i++;
 }
